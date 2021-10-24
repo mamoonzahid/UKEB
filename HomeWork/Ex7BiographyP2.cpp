d2a2067d7ee212db58750dd8dc85ef6bd070dafc
mamoonzahid <mmonzhd80@gmail.com>
2021-10-24 21:06:53 +0400

    Create Ex7BiographyP2.cpp

---
+++ HomeWork/Ex7BiographyP2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age = 18;
+	string name;
+	string hometown;
+	cout << "enter your name: ";
+	cin >> name;
+	cout << "Your hometown? ";
+	cin >> hometown;
+	cout << "Name: " << name << endl;
+	cout << "Age: " << age << endl;
+	cout << "Hometown: " << hometown << endl;
+	return 0;
+}
