a95fe64ef25eb6fa5e2d61ba05c9ec1a4d8bfac9
mamoonzahid <mmonzhd80@gmail.com>
2021-10-24 21:06:57 +0400

    Create Ex7.BiographyP1.cpp

---
+++ HomeWork/Ex7.BiographyP1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name = "Mamoon";
+	int age = 18;
+	string hometown = " lahore ";
+	cout << "NAME: " << name << endl;
+	cout << "age: " << age << endl;
+	cout << "hometown: " << hometown << endl;
+	return 0;
+}
