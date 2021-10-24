743f5db3c96c1e45bfd2cf63f29fc89e3ba9d239
mamoonzahid <mmonzhd80@gmail.com>
2021-10-24 21:07:51 +0400

    Create Ex2.DataTypes.cpp

---
+++ HomeWork/Ex2.DataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+int main() {
+	int firstNumber = 1;
+	cout << firstNumber << endl;
+	bool iCanCode = true;
+	cout << iCanCode << endl;
+	char hopefulGrade = 'a';
+	cout << hopefulGrade << endl;
+	double myDecimal = 1.0;
+	cout << myDecimal << endl;
+	string minimalSentance = "y";
+	cout << minimalSentance << endl;
+	int keyMash = 13213123;
+	cout << keyMash << endl;
+	float mysteryDataType = 5.6f; //Use Google to work this out
+	cin.get(); //keeps console window open in Visual Studio 
+	return 0;
+}
