73a119be784f641c5a2e143c08ae276be982151e
mamoonzahid <mmonzhd80@gmail.com>
2021-10-24 21:08:29 +0400

    Create Ex1.Introduction.cpp

---
+++ HomeWork/Ex1.Introduction.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+	cout << "Name: Mamoon" << endl; 
+	cout << "Age: 18" << endl;
+	cout << "current address : Ajman" << endl;
+	cout << "Permanent address : Lahore" << endl;
+	return 0;
+}
