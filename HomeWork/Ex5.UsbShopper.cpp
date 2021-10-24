f903fbd0246fbb151307bdfa7300510201120e71
mamoonzahid <mmonzhd80@gmail.com>
2021-10-24 21:07:05 +0400

    Create Ex5.UsbShopper.cpp

---
+++ HomeWork/Ex5.UsbShopper.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int total = 50;
+	int cost = 6;
+	cout << "the girl can buy " << total / cost << " USB sticks and will have " << total % cost << " AED remaining." << endl;
+
+	return 0;
+}
