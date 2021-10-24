20957a421d8a8df47094cc1343859957577dc2d5
mamoonzahid <mmonzhd80@gmail.com>
2021-10-24 21:07:11 +0400

    Create Ex4.UntidyCode.cpp

---
+++ HomeWork/Ex4.UntidyCode.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <string> 
+using namespace std;
+int main()
+{
+	cout << "This is untidy code." << endl;
+	cout << "I'm surprised it works." << endl;
+	int number = 6;
+	cout << "It has" << number << "lines of code - " << endl;
+	cout << "each more hideous than the last. " << endl;
+	cout << "You probably should add some line breaks in the text too" << endl;
+    cin.get(); //keeps console window open in Visual Studio 
+	return 0;
+}
