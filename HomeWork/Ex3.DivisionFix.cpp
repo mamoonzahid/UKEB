1ab3b34885e1a6a0828cfbcabcadd5a7814674b8
mamoonzahid <mmonzhd80@gmail.com>
2021-10-24 21:07:13 +0400

    Create Ex3.DivisionFix.cpp

---
+++ HomeWork/Ex3.DivisionFix.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	float numberOne = 50;
+	float numberTwo = 7;
+	cout << numberOne / numberTwo << endl;
+	cin.get(); //keeps console window open in Visual Studio 
+	return 0;
+}
