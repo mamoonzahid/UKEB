e84c21c8914bd0793f6fef1f868a66ce3a1fb562
mamoonzahid <mmonzhd80@gmail.com>
2021-12-20 23:14:03 +0400

    Create risingstars5lines.cpp

---
+++ HomeWork/risingstars5lines.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	for (int i = 1; i < 6; i++)
+	{
+
+		for (int j = i; j > 0; j--)
+		{
+			cout << "*";
+		}
+	cout << endl;
+}
+	return 0;
+}
