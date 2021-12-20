a0ce35a0a696d7a5a022f75b2cfd5af9a1e9e899
mamoonzahid <mmonzhd80@gmail.com>
2021-12-20 23:14:09 +0400

    Create timetravel.cpp

---
+++ HomeWork/timetravel.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	string a[12] = { "jan","feb","mar","apr","may", "jun", "jul", "aug", "sept", "oct", "nov", "dec" };
+	for (i = 0; i <= 11; i++);
+	{
+		cout << a[i] << endl;
+	}
+	return 0;
+}                                     
